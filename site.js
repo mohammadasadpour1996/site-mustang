2144dc308d946e1c6d18d75151d07c473f61b1b6
mohammad <106907251+mohammadasadpour1996@users.noreply.github.com>
2022-07-22 13:49:26 +0430

    site-mustang

---
+++ site.js
@@ -0,0 +1,16 @@
+var num1 =prompt("add your number",0)
+var num2 = prompt("add your number 2",0)
+
+var sum = num1 * num2
+
+if(isNaN( sum == 10)){
+   alert("مقدار درست وارد کنید")
+}
+else if(sum == 11){
+   alert("مقدار 11 میباشد")
+}
+else if(num2 * num3 == 12){
+   alert("مقدار 12 میباشد")
+}else{
+   alert("این مقدار صحیح نیست")
+}
